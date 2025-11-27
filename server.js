c2a64ebdeb6655bb6e6b5354d8337edb594f8abe
unlockedvalle <unlockedvalle@gmail.com>
2025-11-27 20:03:25 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { createServer } from 'node:http';
+
+const bare = createBareServer('/bare/');
+
+const server = createServer();
+
+server.on('request', (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Bare Server ON en puerto ${PORT}`);
+});
